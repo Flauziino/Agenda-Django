626e0bb2e9b2fed7193982e393b5c4181e7d5c30
Lucas Flauzino <27flauzino@gmail.com>
2023-12-07 16:45:53 -0300

    criando campo owner

--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 # Create your models here.
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
@@ -9,6 +9,17 @@ from django.utils import timezone
 # Depois
 # category (foreign key), show (boolean), owner (foreign key)
 # picture (imagem)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
 
 
 class Contact(models.Model):
@@ -20,6 +31,16 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%M/')
+    categeory = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+        )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+        )
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
