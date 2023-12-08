6efaca4cdacd68ea14ab9292d67978eb6e7db6a0
Lucas Flauzino <27flauzino@gmail.com>
2023-12-08 10:36:42 -0300

    tabela

---
+++ contact/views/contact_views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from contact import models
+
+
+def index(request):
+    contacts = models.Contact.objects.all()
+
+    context = {
+        'contacts': contacts,
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context=context
+    )
